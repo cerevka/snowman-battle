b3325f6fd7c9021003845e015ba420543727f1a4
otasandr <otasandr@8785b55e-222c-11df-8cbb-b1dc19da2cd0>
2010-03-13 16:58:03 +0000

    Přidánan implementace třídy KeyboardHandler

--- trunk/controller/keyboardhandler.h
+++ trunk/controller/keyboardhandler.h
@@ -2,16 +2,46 @@
 #define KEYBOARDHANDLER_H
 
 #include <QObject>
+#include <QEvent>
+#include <QKeyEvent>
+
 
 class KeyboardHandler : public QObject
 {
+
 Q_OBJECT
+
 public:
-    explicit KeyboardHandler(QObject *parent = 0);
+
+    explicit KeyboardHandler(QObject * parent = 0);
+
+    void handleKeyEvent(QKeyEvent * const event);
 
 signals:
 
-public slots:
+    void upMove(void);
+    void downMove(void);
+    void leftMove(void);
+    void rightMove(void);
+
+    void stopMove(void);
+
+    void shot(void);
+
+    void changeWeapon(void);
+
+    void pauseGame(void);
+
+private:
+
+    int upKey;
+    int downKey;
+    int leftKey;
+    int rightKey;
+
+    int shotKey;
+    int changeKey;
+    int pauseKey;
 
 };
 
