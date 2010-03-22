e035356b6143d9458016ba5f2af30f44eef09396
otasandr <otasandr@8785b55e-222c-11df-8cbb-b1dc19da2cd0>
2010-03-22 09:23:35 +0000

    Přidán algortimus počítající kolize objektů + přidány atributy třídám Shot a Player

--- model/player.h
+++ model/player.h
@@ -3,10 +3,28 @@
 
 #include "mapobject.h"
 
+// Výčtový typ pro směr hráče
+enum Directions {
+    NORTH, WEST, SOUTH, EAST
+};
+
 class Player : public MapObject
 {
+
 public:
+
     explicit Player(QObject * parent = 0);
+
+    Directions getDirection(void) const;
+    void setDirection(const Directions direction);
+    bool isMoving(void) const;
+    void setMoving(const bool moving);
+
+private:
+
+    Directions direction;
+    bool moving;
+
 };
 
 #endif // PLAYER_H
--- model/shot.cpp
+++ model/shot.cpp
@@ -1,6 +1,16 @@
 #include "shot.h"
 
-Shot::Shot(QObject *parent) :
+Shot::Shot(QObject * parent) :
     QObject(parent)
 {
+}
+
+double Shot::getX(void) const
+{
+    return x;
+}
+
+double Shot::getY(void) const
+{
+    return y;
 }
