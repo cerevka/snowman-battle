05f01285f545af45d581f896cc32b586b36f4df4
tomas.cerevka@gmail.com <tomas.cerevka@gmail.com@8785b55e-222c-11df-8cbb-b1dc19da2cd0>
2010-04-07 17:33:57 +0000

    Vytvareni serveru.

--- controller/network/server.cpp
+++ controller/network/server.cpp
@@ -2,6 +2,22 @@
 
 Server::Server(int port, NetworkInterface *const parent)
 {
+    serverSocket = new QTcpServer(this);
+    // zakaze se pouziti proxy
+    serverSocket->setProxy(QNetworkProxy::NoProxy);
+    // nastavi se maximalni pocet spojeni - pro sest hracu staci pet klientu
+    serverSocket->setMaxPendingConnections(5);
+
+    // nastavi se port pro naslouchani
+    bool listening = serverSocket->listen(QHostAddress::Any, port);
+
+
+    #ifdef _DEBUG_
+      if (listening) {
+        qDebug() << "Server is ready on port " << port << ".";
+      }
+    #endif
+
 }
 
 /**
