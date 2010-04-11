66dc05804bcab7123ebbb417559edd3cffc64600
tomas.cerevka@gmail.com <tomas.cerevka@gmail.com@8785b55e-222c-11df-8cbb-b1dc19da2cd0>
2010-04-11 18:11:28 +0000

    Klienti by meli cist zpravy od serveru, server by mel cist zpravy od klientu.

--- trunk/controller/network/clientthread.cpp
+++ trunk/controller/network/clientthread.cpp
@@ -3,15 +3,32 @@
 ClientThread::ClientThread(QTcpSocket * socket)
 {
     this->socket = socket;
+
+    // vlakno se napoji na signal
+    QObject::connect(socket, SIGNAL(readyRead()), this, SLOT(incomingMessage()));
 }
 
-void ClientThread::run() {
-    while (socket->canReadLine()) {
-        // precte vsechny dostupne byty se socketu
-        message = socket->readAll();
-        // nyni je treba data vyemitovat na slot, kde je prijme parser
+void ClientThread::run()
+{
+    // spusti se smycka udalosti
+    exec();
 
+}
 
-    }
+void ClientThread::incomingMessage()
+{
+    // dostal jsem zpravu, ze je co cist
+    // prectu prvni bajt (delka prichozi informace)
+    // na nulte pozici
+    length = this->socket->read(1).at(0);
 
+    // prectu vsechny znaky patrici k prichozimu packetu
+    message = socket->read(length);
+
+    // vyemituji signal nove prichozi zpravy
+    emit newMessage(message);
+
+    #ifdef _DEBUG_
+    qDebug() << "Network Client Thread: New message";
+    #endif
 }
