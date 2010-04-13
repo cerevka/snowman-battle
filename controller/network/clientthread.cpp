70c259502d0bf7536b09194bd3683d0f83ab802f
tomas.cerevka@gmail.com <tomas.cerevka@gmail.com@8785b55e-222c-11df-8cbb-b1dc19da2cd0>
2010-04-13 06:21:46 +0000

    Zpracovavani packetu davkove - a co lepe - ono to funguje a snehulak serveru se hybe i u klienta. Uz jen napojit emity na parser.

--- controller/network/clientthread.cpp
+++ controller/network/clientthread.cpp
@@ -27,18 +27,18 @@ void ClientThread::incomingMessage()
     // dostal jsem zpravu, ze je co cist
     // prectu prvni bajt (delka prichozi informace)
     // na nulte pozici
-    length = this->socket->read(1).at(0);
-
-    // prectu vsechny znaky patrici k prichozimu packetu
-    QByteArray message = socket->read(length);
+//    length = this->socket->read(1).at(0);
+//
+//    // prectu vsechny znaky patrici k prichozimu packetu
+//    QByteArray message = socket->read(length);
 
     // precte se vsechno, co prislo a nestaram se o obsah
-    //QByteArray message = socket->readAll();
+    QByteArray message = socket->readAll();
 
      qDebug() << "Velikost: " << message.size() << " ukazatel: " << &message;
-//
+
      printMessage(&message);
-//
+
 //    // vyemituji signal nove prichozi zpravy
     emit newMessage(&message);
 
